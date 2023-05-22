cf845e834747d1d96b6561c51c98f9288dbdb177
Aritana Noara Costa Santos <aritanacs@ciandt.com>
2023-05-22 20:43:31 -0300

    end of the course

--- projeto_inicial/admin/service/cliente-service.js
+++ projeto_inicial/admin/service/cliente-service.js
@@ -1,7 +1,62 @@
 const listaClientes = () => {
   return fetch(`http://localhost:3000/profile`).then((resposta) => {
-    return resposta.json();
+    if (resposta.ok) {
+      return resposta.json();
+    }
+    throw new Error('Não foi possível listar os clientes')
   });
 }; //retorna promisse.
 
-export const clienteService = { listaClientes };
+const criaCliente = (nome, email) => {
+  return fetch(`http://localhost:3000/profile`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      nome: nome,
+      email: email,
+    }),
+  }).then((resposta) => {
+    return resposta.body;
+  });
+};
+
+const removeCliente = (id) => {
+  return fetch(`http://localhost:3000/profile/${id}`, {
+    method: "DELETE",
+  }).then(resposta => {
+      if(!resposta.ok){
+        throw new Error('Não foi possível deletar o cliente')
+      }
+  });
+};
+
+const detalhaCliente = (id) => {
+  return fetch(`http://localhost:3000/profile/${id}`).then((resposta) => {
+    return resposta.json();
+  });
+};
+
+const atualizaCliente = (id, nome, email) => {
+  return fetch(`http://localhost:3000/profile/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      nome: nome,
+      email: email,
+    }),
+  }).then((resposta) => {
+    return resposta.json();
+  });
+};
+
+export const clienteService = {
+  listaClientes,
+  criaCliente,
+  removeCliente,
+  detalhaCliente,
+  atualizaCliente,
+};
